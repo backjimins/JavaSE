4de5f3fd9c69724ea69d313d8683849aa8f77e82
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-03-22 19:24:12 +0900

    첫 커밋

---
+++ src/day02/basic/Mainclass01.java
@@ -0,0 +1,29 @@
+package day02.basic;
+
+/*
+ * 주석 - 컴퓨터가 인식하지 않는 영
+ * 
+ * 여러줄 주석 입니다.
+ * 
+ */
+
+
+// 한줄주석
+// 자바 코드 시작 class
+
+
+public class Mainclass01 {
+	// print 메소드 호출 "안녕하세요~" 출력!
+	print();
+}
+
+	/** 
+	 * 안녕하세요 출력 메소드
+	 */
+	public static void main(String[] args) {
+	
+	
+	public static void print() {
+		System.out.println("안녕하세요");
+}
+	}
---
+++ src/day03/loop/MainClass02.java
@@ -0,0 +1,16 @@
+package day03.loop;
+/*
+ *  2. do ~ while()¹®
+ * 
+ * 
+ * 
+ */
+public class MainClass02 {
+		public static void main(String[] args) {
+			int count = 0;
+			do {
+				System.out.println(count);
+				count++;
+			} while(count <100);
+		}
+}
